fcf24797963bf5885c09f41706f517c720ddb2f3
samathaluca <samanthaluca@aol.com>
2019-10-28 09:25:32 +0000

    test heroku

--- .theia/app.py
+++ .theia/app.py
@@ -1,4 +1,5 @@
 import os
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,10 +7,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World ...again'
+    return 'Hello World ...boopie'
 
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
+    app.run(host='0.0.0.0',
+            port=int(os.environ.get("PORT", 5000)),
+            # port=int('5000'),
             debug=True)
