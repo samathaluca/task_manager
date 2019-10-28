9f038e13b819f58abe77fd73a864a742492d4733
samathaluca <samanthaluca@aol.com>
2019-10-28 19:58:30 +0000

    newtry

--- boo.py
+++ boo.py
@@ -23,5 +23,5 @@ if __name__ == '__main__':
 '''
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
+            port=(os.environ.get('PORT')),
             debug=True)
