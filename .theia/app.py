4486f0bad110e997f97a4cde6fc596a11148c119
samathaluca <samanthaluca@aol.com>
2019-10-28 09:35:26 +0000

    going wrong

--- .theia/app.py
+++ .theia/app.py
@@ -7,11 +7,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World ...boopie'
+    return 'Hello World ...boopies'
 
-
+'''
 if __name__ == '__main__':
     app.run(host='0.0.0.0',
             port=int(os.environ.get("PORT", 5000)),
             # port=int('5000'),
+            debug=True)'''
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',
+            port=int('8080'),
             debug=True)
